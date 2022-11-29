ec35e83f012d5ff482970c49846e1b10961595ca
A1exandrN <aanazar0vm1x2@yandex.ru>
2022-11-30 00:46:54 +0300

    fixJsCss

--- scripts/index.js
+++ scripts/index.js
@@ -6,34 +6,40 @@ let closePopup = document.querySelector('.popup__close');
 
 let addOpenPopup = document.querySelector('.popup_opened');
 
-let popupTitle = document.querySelector('#formName').value; //из формы 1
-let popupSubtitle = document.querySelector('#formSpecial').value; //из формы 2
+let popupTitle = document.querySelector('#formName'); //из формы 1
+let popupSubtitle = document.querySelector('#formSpecial'); //из формы 2
 
 let profileTitle = document.querySelector('.profile__title'); //со транички 1
 let profileSubtitle = document.querySelector('.profile__subtitle'); //со транички 2
 
 let popupBottomSave = document.querySelector('.popup__button'); //Кнопка сохранить
 
-popupBottomSave.addEventListener('click', save);
-openPopup.addEventListener('click', open);
-closePopup.addEventListener('click', close);
+function formSubmitHandler(evt) {
+   evt.preventDefault();
 
-function open() {
-   popupBg.classList.add('popup_opened');
+   profileTitle.textContent = popupTitle.value;
+   profileSubtitle.textContent = popupSubtitle.value;
+   close();
 }
 function close() {
    popupBg.classList.remove('popup_opened');
 }
+function open() {
+   popupTitle.value = profileTitle.textContent;
+   popupSubtitle.value = profileSubtitle.textContent;
+   popupBg.classList.add('popup_opened');
+}
+
 function save() {
    popupBg.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
-   evt.preventDefault();
-
-   profileTitle.textContent = popupTitle
-   profileSubtitle.textContent = popupSubtitle
-   save();
-}
-
+//popupBottomSave.addEventListener('click', save);
+openPopup.addEventListener('click', open);
+closePopup.addEventListener('click', close);
 popupForm.addEventListener('submit', formSubmitHandler);
+
+console.log(popupTitle.value)
+console.log(popupSubtitle.value)
+console.log(profileTitle.textContent)
+console.log(profileSubtitle.textContent)
