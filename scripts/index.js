a5c35d8fd96b42dd2cd6171a3f66b3db43d76184
A1exandrN <aanazar0vm1x2@yandex.ru>
2022-11-29 03:10:08 +0300

    fixRewiew

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,39 @@
-// OPEN/CLOSE POPUP
 let popupBg = document.querySelector('.popup'); //фон
-let popupForm = document.querySelector('.popup__container'); //форма
+let popupForm = document.querySelector('.popup__form'); //форма
 
 let openPopup = document.querySelector('.profile__button');
 let closePopup = document.querySelector('.popup__close');
 
 let addOpenPopup = document.querySelector('.popup_opened');
 
+let popupTitle = document.querySelector('#formName').value; //из формы 1
+let popupSubtitle = document.querySelector('#formSpecial').value; //из формы 2
 
-openPopup.addEventListener('click', function () {
-   popupBg.classList.add('popup_opened');
-});
+let profileTitle = document.querySelector('.profile__title'); //со транички 1
+let profileSubtitle = document.querySelector('.profile__subtitle'); //со транички 2
 
-closePopup.addEventListener('click', function () {
-   popupBg.classList.remove('popup_opened');
-});
-
-// SAVE=CLOSE POPUP
 let popupBottomSave = document.querySelector('.popup__button'); //Кнопка сохранить
 
-popupBottomSave.addEventListener('click', function () {
-   popupBg.classList.remove('popup_opened');
-});
+popupBottomSave.addEventListener('click', save);
+openPopup.addEventListener('click', open);
+closePopup.addEventListener('click', close);
 
-// REPLESMENT FORM
+function open() {
+   popupBg.classList.add('popup_opened');
+}
+function close() {
+   popupBg.classList.remove('popup_opened');
+}
+function save() {
+   popupBg.classList.remove('popup_opened');
+}
+
 function formSubmitHandler(evt) {
    evt.preventDefault();
 
-
-   let popupTitle = document.querySelector('#formName').value; //из формы 1
-   let popupSubtitle = document.querySelector('#formSpecial').value; //из формы 2
-
-   let profileTitle = document.querySelector('.profile__title'); //со транички 1
-   let profileSubtitle = document.querySelector('.profile__subtitle'); //со транички 2
-
    profileTitle.textContent = popupTitle
    profileSubtitle.textContent = popupSubtitle
+   save();
 }
 
 popupForm.addEventListener('submit', formSubmitHandler);
---
+++ scripts/index2.js
@@ -0,0 +1,44 @@
+// OPEN/CLOSE POPUP
+let popupBg = document.querySelector('.popup'); //фон
+let popupForm = document.querySelector('.popup__container'); //форма
+
+let openPopup = document.querySelector('.profile__button');
+let closePopup = document.querySelector('.popup__close');
+
+let addOpenPopup = document.querySelector('.popup_opened');
+
+
+openPopup.addEventListener('click', open);
+function open() {
+   popupBg.classList.add('popup_opened');
+}
+
+closePopup.addEventListener('click', close);
+function close() {
+   popupBg.classList.remove('popup_opened');
+}
+
+// SAVE=CLOSE POPUP
+let popupBottomSave = document.querySelector('.popup__button'); //Кнопка сохранить
+
+popupBottomSave.addEventListener('click', save);
+function save() {
+   popupBg.classList.remove('popup_opened');
+}
+
+// REPLESMENT FORM
+function formSubmitHandler(evt) {
+   evt.preventDefault();
+
+
+   let popupTitle = document.querySelector('#formName').value; //из формы 1
+   let popupSubtitle = document.querySelector('#formSpecial').value; //из формы 2
+
+   let profileTitle = document.querySelector('.profile__title'); //со транички 1
+   let profileSubtitle = document.querySelector('.profile__subtitle'); //со транички 2
+
+   profileTitle.textContent = popupTitle
+   profileSubtitle.textContent = popupSubtitle
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
